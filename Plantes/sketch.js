a447b2fc575a9693036ca859333afe546200f728
matheplica <mathieu.heynen@gmail.com>
2025-08-29 13:26:02 +0200

    test

--- Plantes/sketch.js
+++ Plantes/sketch.js
@@ -1,16 +1,16 @@
 let tables = [], numberFile;
 
 function preload() {
-    numberFile = loadStrings('number.txt');  
+    for(let i=0;i<2; i++){
+       tables[i] = loadTable('rapport'+i+'.csv', 'csv', 'header');
+    } 
 }
 function setup() {
 	createCanvas(500, 300);
-  for(let i=0;i<numberFile; i++){
-       tables[i] = loadTable('rapport'+i+'.csv', 'csv', 'header');
-    }
+ 
 }
 function draw(){
-  background(20, 220, 12);
+  background(20, 20, 212);
   textSize(32);
   for(let k = 0; k<tables.length; k++){
     for (let r = 0; r < tables[k].getRowCount(); r++){
